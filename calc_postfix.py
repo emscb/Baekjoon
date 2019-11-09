ef2fb9a7481e9f01c1a9f1cd80f57b96130901bc
Hyeok Min Kwon <khmzxc@naver.com>
2019-11-09 21:37:25 +0900

    계산 단순화

--- calc_postfix.py
+++ calc_postfix.py
@@ -167,27 +167,13 @@ while 1:
 
     # 계산
     oprn = Stack()
-    tmp_oprn = ""
-    is_oprn = False
-    is_float = False
-    for p in postfix:
-        if '0' <= p <= '9':
-            is_oprn = True
-            tmp_oprn += p
-        elif p == '.':
-            is_float = True
-            if not is_oprn:
-                tmp_oprn += '0'
-                is_oprn = True
-            tmp_oprn += p
-        elif p == ' ' and is_oprn:
-            is_oprn = False
-            oprn.push(float(tmp_oprn) if is_float else int(tmp_oprn))
-            is_float = False
-            tmp_oprn = ""
-        elif p in ['+', '-', '*', '/', '^']:
+    P = postfix.split(' ')
+    for p in P:
+        if p in ['+', '-', '*', '/', '^']:
             a2 = oprn.pop()
             a1 = oprn.pop()
             oprn.push(calc(a1, a2, p))
+        else:
+            oprn.push(float(p) if '.' in p else int(p))
     print(oprn.view())
     del oprn
